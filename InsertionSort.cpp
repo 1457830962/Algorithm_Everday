268ed64aca02aa378d6571fab094cf0b1d547139
zxm <1457830962@qq.com>
2019-05-16 21:53:26 +0800

    实例：进行了修改

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,33 +1,76 @@
 #include <QCoreApplication>
-#include <iostream>
+#include<iostream>
+#include <vector>
 using namespace std;
-//插入排序
+
+
+
+///***************插入排序(写法一)***************///
+void InsertSort(int a[], int n)
+{
+    for (int j = 1; j < n; j++)  //思考这里j为啥从1开始  因为有一个元素是先排好序的
+    {
+
+        int key = a[j]; //待排序第一个元素
+        int i = j - 1;  //代表已经排过序的元素最后一个索引数
+        while (i >= 0 && key < a[i])
+        {
+            //从后向前逐个比较已经排序过数组，如果比它小，则把后者用前者代替，
+            //其实说白了就是数组逐个后移动一位,为找到合适的位置时候便于Key的插入
+            a[i + 1] = a[i];
+            i--;
+        }
+        a[i + 1] = key;//找到合适的位置了，赋值,在i索引的后面设置key值。
+    }
+}
+
+///***************插入排序(写法二)***************///
+void InsertSort2(vector<int> &num){
+    for(int i = 1;i < num.size();++i){
+        for(int j = i;j > 0;--j){
+            if(num[j] < num[j - 1]){
+                int temp = num[j];
+                num[j] = num[j-1];
+                num[j-1] = temp;
+            }
+        }
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication aa(argc, argv);
-    /**************************************************************************
-**插入排序的原理：始终定义第一个元素为 有序的，将元素逐个插入到有序排列之中，其特点 
-**是要不断的移动数据，空出一个适当的位置，把待插入的元素放到里面去
-****************************************************************************/
-    int a[]={4,0,2,3,1},i,j,t;
-    for(i=1;i<5;i++)
-    {
-        t=a[i];
-        j=i-1;
-        while(j>=0&&t>a[j])
-        {
-            a[j+1]=a[j];
-            --j;
-        }
-        a[j+1]=t;
-    }
-    printf("a数组：");
-        for (int i = 0; i < 5; i++)
-        {
-            printf(" %d ", a[i]);
-        }
-        printf("\n");
-    return aa.exec();
-}
+    QCoreApplication a(argc, argv);
 
+    int d[] = { 30,20,10,25,15,28 };
+    cout << "输入数组  { 30,20,10,25,15,28 } " << endl;
+    vector<int> v;
+    v.push_back(12);
+    v.push_back(15);
+    v.push_back(9);
+    v.push_back(20);
+    v.push_back(6);
+    v.push_back(31);
+    v.push_back(24);
+
+    InsertSort(d,6);
+//    InsertSort2(v);
+    cout << "排序后结果：";
+    //***利用下标遍历数组***//
+//    for (int i = 0; i < 7; i++)
+//    {
+//        cout << v[i]<<" ";
+//    }
+    //***利用***//
+    vector<int>::const_iterator iterator = v.begin();
+      for(;iterator != v.end();iterator++)
+      {
+         cout<<*iterator<<" ";
+
+      }
+
+
+
+
+    return a.exec();
+}
